3e8559aeeae10f3cb04c59c86cb026a0a7ef8247
binnazcabuk <binnazcabuk9@gmail.com>
2021-09-15 20:56:13 +0300

    gallery view added

---
+++ Business/Concrete/ImageFileManager.cs
@@ -0,0 +1,26 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ImageFileManager : IImageFileService
+    {
+       private IImageFileDal _imageDal;
+
+        public ImageFileManager(IImageFileDal imageDal)
+        {
+            _imageDal = imageDal;
+        }
+
+        public List<ImageFile> GetAll()
+        {
+           return _imageDal.List();
+        }
+    }
+}
---
+++ MvcProject/Controllers/GalleryController.cs
@@ -0,0 +1,22 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProject.Controllers
+{
+    public class GalleryController : Controller
+    {
+        // GET: Gallery
+
+        ImageFileManager _imageFile = new ImageFileManager(new EfImageFileDal());
+        public ActionResult Index()
+        {
+            var files = _imageFile.GetAll();
+            return View(files);
+        }
+    }
+}
